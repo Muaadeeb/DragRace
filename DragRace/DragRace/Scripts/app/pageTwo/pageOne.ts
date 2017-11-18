5ab58b1e0a737f332c7b2401ff2d6e2dc5b7d76c
RitzBitz2323 <ritvikemail@gmail.com>
2017-11-18 11:35:29 -0600

    #0000_Initial COmmit

---
+++ DragRace/DragRace/Scripts/app/pageTwo/pageOne.ts
@@ -0,0 +1,50 @@
+﻿
+let randomNumberGenerator = (min, max) => {
+    let someValue = Math.random() * (max - min) + min;
+    return Math.round(someValue);
+}
+
+//for (var i = 1; i <= 10; i++) {
+//    document.write(Math.random());
+//    document.write("51\r\n");
+//}
+let LOL = 56;
+let winChance = () => {
+    let lostPoint3 = LOL - 3;
+    let losePoint6 = LOL - 6;
+    let winPoint3 = LOL + 3;
+    let winPoint6 = LOL + 6;
+
+}
+    //document.getElementById("pointsSpent").innerHTML = totalCCP.toString();
+    //switch (randomNumberGenerator) {
+    //    case (randomNumberGenerator < .25):
+    //        alert("Lose 6");
+    //        break;
+    //    case (randomNumberGenerator > .26 && x < .5):
+    //        alert("Lose 3 ");
+    //        break;
+    //    case (randomNumberGenerator > .51 && x < .75):
+    //        alert("Win 3");
+    //        break;
+    //    case (randomNumberGenerator > .76 && x < 1):
+    //        alert("Win 6");
+    //        break;
+    //    default:
+    //        alert("none");
+    //        break;
+    //}
+
+  // if (totalCCP > 100) {
+   //     alert("You have over spent.  Please adjust your point spend.");
+  //}
+
+    
+//for (var i = 1; i >= 0; i++) {
+//    document.write(Math.random());
+//}
+
+// <div class="margintop">
+//      <div class = "row">
+//<span class="col-md-3"><strong>First Name</strong></span>
+//    </div>
