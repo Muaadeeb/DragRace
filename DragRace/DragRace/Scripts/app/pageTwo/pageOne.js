81f180f0c1c4d632c666f0cf398cd26bf03b273d
RitzBitz2323 <ritvikemail@gmail.com>
2017-11-18 11:58:24 -0600

    ExperimentInClassMathRandom 11/18

--- DragRace/DragRace/Scripts/app/pageTwo/pageOne.js
+++ DragRace/DragRace/Scripts/app/pageTwo/pageOne.js
@@ -1,4 +1,4 @@
-var randomNumberGenerator = function (min, max) {
+var randNumGen = function (min, max) {
     var someValue = Math.random() * (max - min) + min;
     return Math.round(someValue);
 };
@@ -6,7 +6,7 @@ var randomNumberGenerator = function (min, max) {
 //    document.write(Math.random());
 //    document.write("51\r\n");
 //}
-var LOL = 56;
+var LOL = (1, 100);
 var winChance = function () {
     var lostPoint3 = LOL - 3;
     var losePoint6 = LOL - 6;
@@ -14,23 +14,24 @@ var winChance = function () {
     var winPoint6 = LOL + 6;
 };
 //document.getElementById("pointsSpent").innerHTML = totalCCP.toString();
-//switch (randomNumberGenerator) {
-//    case (randomNumberGenerator < .25):
-//        alert("Lose 6");
-//        break;
-//    case (randomNumberGenerator > .26 && x < .5):
-//        alert("Lose 3 ");
-//        break;
-//    case (randomNumberGenerator > .51 && x < .75):
-//        alert("Win 3");
-//        break;
-//    case (randomNumberGenerator > .76 && x < 1):
-//        alert("Win 6");
-//        break;
-//    default:
-//        alert("none");
-//        break;
-//}
+switch (randNumGen) {
+    case (randNumGen < 25):
+        LOL - 6;
+        break;
+    case (randNumGen > 26 && x < 5):
+        LOL - 3;
+        break;
+    case (randNumGen > 51 && x < 75):
+        LOL + 3;
+        break;
+    case (randNumGen > 76 && x < 100):
+        LOL + 6;
+        break;
+}
+if (LOL > 100) {
+    alert("How did you do this?!");
+}
+;
 // if (totalCCP > 100) {
 //     alert("You have over spent.  Please adjust your point spend.");
 //}
@@ -41,4 +42,3 @@ var winChance = function () {
 //      <div class = "row">
 //<span class="col-md-3"><strong>First Name</strong></span>
 //    </div>
-//# sourceMappingURL=pageOne.js.map
