856245b1dc12326f88a85cd8efb2cf454d200dda
AgntBluSqurrl <zed@zedmain.com>
2018-03-10 11:06:16 -0600

    Page one

--- DragRace/DragRace/Scripts/app/pageTwo/pageOne.ts
+++ DragRace/DragRace/Scripts/app/pageTwo/pageOne.ts
@@ -1,41 +1,46 @@
-﻿ let randNumGen = (min, max) => {
+﻿let nameAge = () => {
 
-     let someValue = Math.random() * (max - min) + min;
-    return Math.round(someValue);
 }
+
+
+// let randNumGen = (min, max) => {
+
+//     let someValue = Math.random() * (max - min) + min;
+//    return Math.round(someValue);
+//}
 
 //for (var i = 1; i <= 10; i++) {
 //    document.write(Math.random());
 //    document.write("51\r\n");
 //}
-let LOL = (1,100) ;
-let winChance = () => {
-    let lostPoint3 = LOL - 3;
-    let losePoint6 = LOL - 6;
-    let winPoint3 = LOL + 3;
-    let winPoint6 = LOL + 6;
+//let LOL = (1,100) ;
+//let winChance = () => {
+//    let lostPoint3 = LOL - 3;
+//    let losePoint6 = LOL - 6;
+//    let winPoint3 = LOL + 3;
+//    let winPoint6 = LOL + 6;
 
-}
+//}
 
    //document.getElementById("pointsSpent").innerHTML = totalCCP.toString();
 
-switch (randNumGen) {
-    case (randNumGen < 25):
-            LOL - 6;
-            break;
-    case (randNumGen > 26 && x < 5):
-        LOL - 3;
-            break;
-    case (randNumGen > 51 && x < 75):
-        LOL + 3;
-            break;
-    case (randNumGen > 76 && x < 100):
-            LOL + 6;
-           break;
-}
-if (LOL > 100) {
-    alert("How did you do this?!")
-};
+//switch (randnumgen) {
+//    case (randnumgen < 25):
+//            lol - 6;
+//            break;
+//    case (randnumgen > 26 && x < 5):
+//        lol - 3;
+//            break;
+//    case (randnumgen > 51 && x < 75):
+//        lol + 3;
+//            break;
+//    case (randnumgen > 76 && x < 100):
+//            lol + 6;
+//           break;
+//}
+//if (lol > 100) {
+//    alert("how did you do this?!")
+//};
   // if (totalCCP > 100) {
    //     alert("You have over spent.  Please adjust your point spend.");
   //}
