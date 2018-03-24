ff6550979960ddbc763ce58fdc65b3140523d622
AgntBluSqurrl <zed@zedmain.com>
2018-03-24 11:00:16 -0500

    Session Storage

---
+++ DragRace/DragRace/Scripts/Old/oldOne.js
@@ -0,0 +1,48 @@
+//let nameAge = () => {
+//}
+//SDIAOJGDSGAsfdfsdfsfsfsdf
+/////////// let randNuMGen = (min, max) => {
+//     let someValue = Math.random() * (max - min) + min;
+//    return Math.round(someValue);
+//}
+//More changesdasfkl
+//for (var i = 1; i <= 10; i++) {
+//    document.write(Math.random());
+//    document.write("51\r\n");
+//}
+//let LOL = (1,100) ;
+//let winChance = () => {
+//    let lostPoint3 = LOL - 3;
+//    let losePoint6 = LOL - 6;afgsedfhdfh
+//    let winPoint3 = LOL + 3;
+//    let winPoint6 = LOL + 6;
+//}
+//document.getElementById("pointsSpent").innerHTML = totalCCP.toString();
+//switch (randnumgen) {
+//    case (randnumgen < 25):
+//            lol - 6;
+//            break;
+//    case (randnumgen > 26 && x < 5):
+//        lol - 3;
+//            break;
+//    case (randnumgen > 51 && x < 75):
+//        lol + 3;
+//            break;
+//    case (randnumgen > 76 && x < 100):
+//            lol + 6;
+//           break;
+//}
+//if (lol > 100) {
+//    alert("how did you do this?!")
+//};
+// if (totalCCP > 100) {
+//     alert("You have over spent.  Please adjust your point spend.");
+//}
+//for (var i = 1; i >= 0; i++) {
+//    document.write(Math.random());
+//}
+// <div class="margintop">
+//      <div class = "row">
+//<span class="col-md-3"><strong>First Name</strong></span>
+//    </div>
+//# sourceMappingURL=oldOne.js.map
--- DragRace/DragRace/Scripts/app/pageTwo/pageOne.js
+++ DragRace/DragRace/Scripts/app/pageTwo/pageOne.js
@@ -1,44 +1,31 @@
-var randNumGen = function (min, max) {
-    var someValue = Math.random() * (max - min) + min;
-    return Math.round(someValue);
-};
-//for (var i = 1; i <= 10; i++) {
-//    document.write(Math.random());
-//    document.write("51\r\n");
-//}
-var LOL = (1, 100);
-var winChance = function () {
-    var lostPoint3 = LOL - 3;
-    var losePoint6 = LOL - 6;
-    var winPoint3 = LOL + 3;
-    var winPoint6 = LOL + 6;
-};
-//document.getElementById("pointsSpent").innerHTML = totalCCP.toString();
-switch (randNumGen) {
-    case (randNumGen < 25):
-        LOL - 6;
-        break;
-    case (randNumGen > 26 && x < 5):
-        LOL - 3;
-        break;
-    case (randNumGen > 51 && x < 75):
-        LOL + 3;
-        break;
-    case (randNumGen > 76 && x < 100):
-        LOL + 6;
-        break;
+var DRCharacter = new drCharacter(null, null, null);
+function drCharacter(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
 }
-if (LOL > 100) {
-    alert("How did you do this?!");
-}
-;
-// if (totalCCP > 100) {
-//     alert("You have over spent.  Please adjust your point spend.");
+//let adjustCharacterCreationPoints = () => {
+//    let totalCCP = totalCharacterCreationPointsSpent();
+//    document.getElementById(" pointsSpent").innerHTML = totalCCP.toString();
+//    if (totalCCP > 100) {
+//        alert("You have over spent.  Please adjust your point spend.");
+//    }
 //}
-//for (var i = 1; i >= 0; i++) {
-//    document.write(Math.random());
+//let totalCharacterCreationPointsSpent = () => {
+//    let ccp1 = parseInt(document.getElementById("combatOffense")["value"]);
+//    let ccp2 = parseInt(document.getElementById("combatDefense")["value"]);
+//    let ccp3 = parseInt(document.getElementById("luck")["value"]);
+//    let ccp4 = parseInt(document.getElementById("gamble")[" value"]);
+//    let ccp5 = parseInt(document.getElementById("hack")["value"]);
+//    let ccp6 = parseInt(document.getElementById("health")[" value"]);
+//    return ccp1 + ccp2 + ccp3 + ccp4 + ccp5 + ccp6;
 //}
-// <div class="margintop">
-//      <div class = "row">
-//<span class="col-md-3"><strong>First Name</strong></span>
-//    </div>
+var nameStorage = function () {
+    var firstName = document.getElementById("fName")["value"];
+    var lastName = document.getElementById("lName")["value"];
+    var age = parseInt(document.getElementById("age")["value"]);
+    var DRCharacter = new drCharacter(firstName, lastName, age);
+    sessionStorage.setItem("Name", JSON.stringify(DRCharacter));
+    window.location.href = "pageOne.html";
+};
+//# sourceMappingURL=pageOne.js.map
